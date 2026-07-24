a3a06eaa61f5c49d54890da2c4984fc0204e8eb5
Jyoti-kamble1 <jyoti.kamble9828@gmail.com>
2026-07-24 16:30:11 +0530

    file utility added

---
+++ Orange_Health_1.1/src/main/java/com/ty/orangehealth/Generic_Utility/FileUtility.java
@@ -0,0 +1,57 @@
+package com.ty.orangehealth.Generic_Utility;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Properties;
+
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.ss.usermodel.WorkbookFactory;
+
+public class FileUtility 
+{
+	public String getDataFromProperties(String key)
+	{
+		try {
+			FileInputStream fis = new FileInputStream("C:\\Users\\Jyoti\\git\\Repo_OrangeHealthJyoti\\Orange_Health_1.1\\Test-Data\\OH_CD.properties");
+			Properties pobj=new Properties();
+			pobj.load(fis);
+			String data = pobj.getProperty(key);
+			return data;	
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return null;
+	}
+	
+	public String getDataFromExcel(String path,String sheet,int rownum,int cellnum)
+	{String data="";
+		try {
+			FileInputStream fis =new FileInputStream(path);
+			Workbook wb=WorkbookFactory.create(fis);
+			Sheet sh = wb.getSheet(sheet);
+			data = sh.getRow(rownum).getCell(cellnum).toString();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return data;	
+	}
+	
+	public String getDataFromExcel(String sheet,int rownum,int cellnum)
+	{String data="";
+		try {
+			FileInputStream fis =new FileInputStream("C:\\Users\\Jyoti\\git\\Repo_OrangeHealthJyoti\\Orange_Health_1.1\\Test-Data\\TestScriptData.xlsx");
+			Workbook wb=WorkbookFactory.create(fis);
+			Sheet sh = wb.getSheet(sheet);
+			data = sh.getRow(rownum).getCell(cellnum).toString();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return data;	
+	}
+
+}
--- Orange_Health_1.1/src/test/java/Location_Module/Location.java
+++ Orange_Health_1.1/src/test/java/Location_Module/Location.java
@@ -1,5 +1,19 @@
 package Location_Module;
 
-public class Location {
+import com.ty.orangehealth.Generic_Utility.FileUtility;
+
+public class Location 
+{
+	public static void main(String[] args) 
+	{
+		FileUtility fu=new FileUtility();
+		String data = fu.getDataFromProperties("url");
+		System.out.println(data);	
+		
+		String data1 = fu.getDataFromExcel("Sheet1", 0, 0);
+		double num = Double.parseDouble(data1);
+		System.out.println(num);
+		
+	}
 
 }
