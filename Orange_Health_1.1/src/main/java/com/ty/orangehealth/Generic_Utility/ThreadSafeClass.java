4601d1ba346c96adc98a83265b0516065533525d
kiran2816 <pawarkiran199728@gmail.com>
2026-07-24 17:55:25 +0530

    thread safe class added

--- Orange_Health_1.1/src/main/java/com/ty/orangehealth/Generic_Utility/ThreadSafeClass.java
+++ Orange_Health_1.1/src/main/java/com/ty/orangehealth/Generic_Utility/ThreadSafeClass.java
@@ -1,5 +1,29 @@
 package com.ty.orangehealth.Generic_Utility;
 
+import org.openqa.selenium.WebDriver;
+
+import com.aventstack.extentreports.ExtentTest;
+
 public class ThreadSafeClass {
+	public static ThreadLocal<WebDriver> driver = new ThreadLocal<>();
+	public static ThreadLocal<ExtentTest> test = new ThreadLocal<ExtentTest>();
 	
+	public static ExtentTest getTest() {
+		return test.get();
+	}
+	
+	public static void setTest(ExtentTest actTest) {
+		test.set(actTest);
+	}
+	
+	public static WebDriver getDriver() {
+		return driver.get();
+	}
+	
+	
+	public static void setDriver(WebDriver actDriver) {
+		driver.set(actDriver);
+	}
 }
+
+
