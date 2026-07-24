5a85b8a8aab9b3a4fed20386306e30fc85185596
kiran2816 <pawarkiran199728@gmail.com>
2026-07-24 16:43:05 +0530

    base class updated

--- Orange_Health_1.1/src/main/java/com/ty/orangehealth/business_utility/BaseClassTest.java
+++ Orange_Health_1.1/src/main/java/com/ty/orangehealth/business_utility/BaseClassTest.java
@@ -1,9 +1,23 @@
 package com.ty.orangehealth.business_utility;
 
+import java.time.Duration;
+
+
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.safari.SafariDriver;
+import org.testng.annotations.AfterClass;
 import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeSuite;
+import org.testng.xml.XmlTest;
+
 
 public class BaseClassTest {
+	WebDriver driver;
 	@BeforeSuite
 	public void configBS() {
 		System.out.println("DB coonection created");
@@ -12,6 +26,45 @@ public class BaseClassTest {
 	@AfterSuite
 	public void configAS() {
 		System.out.println("DB coonection closed");
+	}
+	
+	@BeforeClass
+	public void configBC(XmlTest test) {
+//		WebDriverUtility proUtil = new WebDriverUtility();
+		String browser = System.getProperty("browser");
+		if(browser == null || browser.isBlank()) {
+			browser = test.getParameter("browser");
+			if(browser == null || browser.isBlank()) {
+				browser = "chrome";
+			}
+		}
+		
+		if(browser.equalsIgnoreCase("chrome")) {
+			driver = new ChromeDriver();
+		}
+		else if(browser.equalsIgnoreCase("safari")) {
+			driver = new SafariDriver();
+		}
+		else if(browser.equalsIgnoreCase("edge")) {
+			driver = new EdgeDriver();
+		}
+		else if(browser.equalsIgnoreCase("firefox")) {
+			driver = new FirefoxDriver();
+		}else {
+			throw new IllegalArgumentException("Invalid browser : " + browser);
+		}
+		
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
+		driver.get("https://www.orangehealth.in/");
+		System.out.println("before class");
+		
+	}
+	
+	
+	@AfterClass
+	public void configAC() {
+		driver.quit();
 	}
 	
 }
